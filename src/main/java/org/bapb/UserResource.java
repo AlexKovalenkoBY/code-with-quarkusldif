244c428fb4561b82178cc0e2ce10e38797c6f678
AlexKovalenkoBY <kovalenkoam@gmail.com>
2024-07-26 09:14:32 +0300

    ldiff added

---
+++ src/main/java/org/bapb/UserResource.java
@@ -0,0 +1,21 @@
+package org.bapb;
+
+
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.SecurityContext;
+
+@Path("/api/users")
+public class UserResource {
+
+    @GET
+    @RolesAllowed("standardRole")
+    @Path("/me")
+    public String me(@Context SecurityContext securityContext) {
+        return securityContext.getUserPrincipal().getName();
+    }
+}
